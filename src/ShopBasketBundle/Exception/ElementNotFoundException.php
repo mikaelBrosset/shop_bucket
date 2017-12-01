25d419e623da0a3d989e6ef50c846b61f3e1346c
mikaelBrosset <mikael.brosset@gmail.com>
2017-12-02 00:02:11 +0100

    Exception created

---
+++ src/ShopBasketBundle/Exception/ElementNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: Mikael Brosset
+ * Email: mikael.brosset@gmail.com
+ * Date: 01/12/17
+ */
+namespace ShopBasketBundle\Exception;
+
+class ElementNotFoundException extends \Exception
+{
+    public function __construct($className, $elementName)
+    {
+        parent::__construct(sprintf("Element %s not found in %s", $elementName, $className));
+    }
+}
